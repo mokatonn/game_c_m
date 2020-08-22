e041bf54a4f67d037b72e60af1481434e109f9dc
CarlosVazPI <carlos.vazpi@gmail.com>
2020-08-22 13:44:26 +0100

    Fixed function

--- movePlayer.js
+++ movePlayer.js
@@ -11,8 +11,8 @@ function getPossibleMoves (character){
   const x = character.position.x
   const y = character.position.y
 
-  return [{x, y: y-1}, {x, y:y+1}, {x: x-1, y}, {x:x+1, y}].reduce((acc, move) => {
-    const isOutOfBounds = x < 0 || y < 0 || x >= map_movement[0].length || y >= map_movement.length || !map_movement[x][y]
+  return [{x, y: y-1}, {x, y:y+1}, {x: x-1, y}, {x:x+1, y}].reduce((acc, { x, y }) => {
+    const isOutOfBounds = x < 0 || y < 0 || x >= map_movement[0].length || y >= map_movement.length
     const isOtherPlayer = map_player[x][y] && map_player[x][y].playerId !== character.playerId
     if (isOutOfBounds || !map_movement[x][y]) return acc
     const moveType = isOtherPlayer ? 'attack' : 'movement'
